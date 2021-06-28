a831ecee979460f0623d197975b861a71d3128ec
Han Yoonjae <han051597@naver.com>
2021-06-28 14:42:09 +0900

    Initial commit

---
+++ src/main/java/ExerMate/ExerMate/ExerMateApplication.java
@@ -0,0 +1,13 @@
+package ExerMate.ExerMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExerMate/ExerMate/ExerMateApplicationTests.java
@@ -0,0 +1,13 @@
+package ExerMate.ExerMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
